9611253dcbef0c9d59c7704c7f6a60bee46dd5e9
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-01-28 19:30:54 +0900

    env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use migration::{generator::generate_sql, migration::insert_data};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
     let generated_sql_path = generate_sql()?;
     insert_data(generated_sql_path)?;
 
